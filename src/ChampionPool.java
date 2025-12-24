1250d80e666be7bed7151b65a3f281dc76487c60
soomin0209 <soomin02094@gmail.com>
2025-12-24 14:18:11 +0900

    feat: ChampionPool(Map) 구현 (필수-4)

---
+++ src/ChampionPool.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class ChampionPool {
+
+    private Map<String, Champion> pool = new HashMap<>();
+
+    public void addChampion(Champion champion) {
+        pool.put(champion.getName(), champion);
+    }
+
+    public Optional<Champion> find(String name) {
+        return Optional.ofNullable(pool.get(name));
+    }
+}
