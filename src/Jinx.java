2697bc57130b8139ddf294674127548420ae3d82
soomin0209 <soomin02094@gmail.com>
2025-12-24 20:50:57 +0900

    refactor: Jinx 남은 마나 출력 순서 변경

--- src/Jinx.java
+++ src/Jinx.java
@@ -23,9 +23,9 @@ public class Jinx extends Champion implements ManaUser {
 
         System.out.println(getName() + "의 휘릭휘릭!(Q)");
         checkMana(getName());
-        System.out.println("공격력 15 증가, 방어력 5 감소");
         consumeMana(20);
         System.out.println("(남은 마나: " + getMana() + ")");
+        System.out.println("공격력 15 증가, 방어력 5 감소");
         setAttackDamage(getAttackDamage() + 15);
         setDefense(getDefense() - 5);
         System.out.println("(현재 공격력: " + getAttackDamage() + ", 현재 방어력: " + getDefense() + ")\n");
